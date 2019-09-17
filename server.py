06658edfecaa198a1d9a21bc21c3e09a6512b76c
andreufsm <dias.andrepaz@gmail.com>
2019-09-17 16:49:45 -0300

    finalizado

--- server.py
+++ server.py
@@ -1,2 +1,8 @@
-request = requests.get('http://api.open-notify.org')
-print(request.text)
+import requests
+
+astronautas = requests.get('http://api.open-notify.org/astros.json')
+astronautas_json  = astronautas.json()
+print("Atualmente existem %s astronautas no espaço \n" % (astronautas_json['number']))
+
+for a in astronautas_json['people']:
+    print("Astronauta: %s " % (a['name']))
